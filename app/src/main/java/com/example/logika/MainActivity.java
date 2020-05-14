d68e9a579aa5c946347d41c9cab6e6fb09167441
pisonmison <58531355+pisonmison@users.noreply.github.com>
2020-05-15 00:04:24 +0200

    UPDATE: Dynamisches checken implementiert
     disablen von buttons funktioniert zu 50%
     -> jetzt in checkPins();

--- app/src/main/java/com/example/logika/MainActivity.java
+++ app/src/main/java/com/example/logika/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.logika;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -14,39 +16,41 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Arrays;
 
-public class MainActivity<pinArray> extends AppCompatActivity implements View.OnClickListener, RadioGroup.OnCheckedChangeListener, CheckBox.OnCheckedChangeListener {
+public class MainActivity<pinArray> extends AppCompatActivity implements View.OnClickListener, RadioGroup.OnCheckedChangeListener {
 
-    Boolean[] pinArray = {false, false, false, false};
-
-
-    Boolean[] colorButtonsArray = {false, false, false, false, false, false};
-
-    int attemptCounter;         //counter für textview
-
-    Backend backend;
-
-    CheckBox[] chk;
+   private Boolean[] pinArray = {false, false, false, false};
 
 
-    CheckBox checkBoxA;
-    CheckBox checkBoxB;
-    CheckBox checkBoxC;   //checkbox buttons
-    CheckBox checkBoxD;
+   private Boolean[] colorButtonsArray = {false, false, false, false, false, false};
 
-    RadioGroup radioGroup;
-    RadioButton radioButtonRed;
-    RadioButton radioButtonGreen;
-    RadioButton radioButtonBlue;
-    RadioButton radioButtonYellow;
-    RadioButton radioButtonCyan;
-    RadioButton radioButtonMagenta;
+   private int attemptCounter;         //counter für textview
 
-    FloatingActionButton mailButton;
+   private Backend backend;
 
-    TextView a;
-    TextView b;
-    TextView c;
-    TextView d;
+   private CheckBox[] chk;
+
+   private LinearLayout linearLayout;
+   private CheckBox checkBoxA;
+   private CheckBox checkBoxB;
+   private CheckBox checkBoxC;   //checkbox buttons
+   private CheckBox checkBoxD;
+
+   private RadioGroup radioGroup;
+   private RadioButton radioButtonRed;
+   private RadioButton radioButtonGreen;
+   private RadioButton radioButtonBlue;
+   private RadioButton radioButtonYellow;
+   private RadioButton radioButtonCyan;
+   private RadioButton radioButtonMagenta;
+
+   private FloatingActionButton mailButton;
+   private RadioButton radioButton;
+
+   private TextView a;
+   private TextView b;
+   private TextView c;
+   private TextView d;
+
 
 
     @Override
@@ -60,13 +64,15 @@ public class MainActivity<pinArray> extends AppCompatActivity implements View.On
         checkBoxD = findViewById(R.id.checkBoxDID);
 
 
+        linearLayout = findViewById(R.id.linearLayout_checkboxen_ID);
         radioGroup = (RadioGroup) findViewById(R.id.radioGroupID);
-        radioButtonRed = findViewById(R.id.radioButton1);
-        radioButtonGreen = findViewById(R.id.radioButton2);
-        radioButtonBlue = findViewById(R.id.radioButton3);
-        radioButtonYellow = findViewById(R.id.radioButton4);
-        radioButtonCyan = findViewById(R.id.radioButton5);
-        radioButtonMagenta = findViewById(R.id.radioButton6);
+
+        radioButtonRed = findViewById(R.id.radioButtonRED_ID);
+        radioButtonGreen = findViewById(R.id.radioButtonGREEN_ID);
+        radioButtonBlue = findViewById(R.id.radioButtonBLUE_ID);
+        radioButtonYellow = findViewById(R.id.radioButtonYELLOW_ID);
+        radioButtonCyan = findViewById(R.id.radioButtonCYAN_ID);
+        radioButtonMagenta = findViewById(R.id.radioButtonMAGENTA_ID);
 
         a = findViewById(R.id.textViewA_ID);
         b = findViewById(R.id.textViewB_ID);
@@ -79,73 +85,71 @@ public class MainActivity<pinArray> extends AppCompatActivity implements View.On
         checkBoxD.setOnClickListener(this);
 
         radioGroup.setOnCheckedChangeListener(this);
+        disableButtons();
 
 
     }
 
-
-    public void checkColor() {
-
-        if (radioButtonRed.isChecked()) {
-            colorButtonsArray[0] = true;
-            clearColorArray(0);
-        } else if (radioButtonGreen.isChecked()) {
-            colorButtonsArray[1] = true;
-            clearColorArray(1);
-        } else if (radioButtonBlue.isChecked()) {
-            colorButtonsArray[2] = true;
-            clearColorArray(2);
-        } else if (radioButtonYellow.isChecked()) {
-            colorButtonsArray[3] = true;
-            clearColorArray(3);
-        } else if (radioButtonCyan.isChecked()) {
-            colorButtonsArray[4] = true;
-            clearColorArray(4);
-
-        } else if (radioButtonMagenta.isChecked()) {
-            colorButtonsArray[5] = true;
-            clearColorArray(5);
-
+    //iterate through radiogroup and dissable all buttons/same with enable
+    public void disableButtons() {
+        for (int i = 0; i < radioGroup.getChildCount(); i++) {
+            ((RadioButton) radioGroup.getChildAt(i)).setEnabled(false);
         }
-
     }
 
-    public void checkPins() {
-        if (checkBoxA.isChecked()) {
-            pinArray[0] = true;
-        } else {
-            pinArray[0] = false;
+    public void enableButtons() {
+        for (int i = 0; i < radioGroup.getChildCount(); i++) {
+            ((RadioButton) radioGroup.getChildAt(i)).setEnabled(true);
         }
-        if (checkBoxB.isChecked()) {
-            pinArray[1] = true;
-        } else {
-            pinArray[1] = false;
-        }
-        if (checkBoxC.isChecked()) {
-            pinArray[2] = true;
-        } else {
-            pinArray[2] = false;
-        }
-        if (checkBoxD.isChecked()) {
-            pinArray[3] = true;
-        } else {
-            pinArray[3] = false;
-        }
-
-
     }
 
+    public void setColor() {
 
-    public CheckBox checkPinArray() {
-        for (int i = 0; i < 3; i++) {
-            if (pinArray[0] == true) {
-                return checkBoxA;
+        //iterate throught radiogroup and check which button is clicked, set array accordingly
+        for (int i = 0; i < radioGroup.getChildCount(); i++) {
+            if (((CompoundButton) radioGroup.getChildAt(i)).isChecked()) {
+                colorButtonsArray[i] = true;
+            } else {
+                colorButtonsArray[i] = false;
             }
         }
-        return null;
-    }
 
-    public void chooseColor(TextView v){
+        }
+
+
+        //interate thoough the linear layout and check pins and set the pinArray accordingly
+        public void checkPins () {
+
+
+
+        for (int i = 0; i < linearLayout.getChildCount(); i++) {
+
+                if (((CompoundButton) linearLayout.getChildAt(i)).isChecked()) {
+                    pinArray[i] = true;
+                    enableButtons();
+
+                }
+                else if(((CompoundButton) linearLayout.getChildAt(0)).isChecked() == false ||
+                        ((CompoundButton) linearLayout.getChildAt(1)).isChecked()== false  ||
+                        ((CompoundButton) linearLayout.getChildAt(2)).isChecked()== false  ||
+                        ((CompoundButton) linearLayout.getChildAt(3)).isChecked()== false){
+
+                    disableButtons();
+
+                }
+
+                else {
+                    pinArray[i] = false;
+                }
+
+            }
+        }
+
+
+
+
+        //choose which color to color the pin(textviev v) given by function choosePin();
+        public void chooseColor (TextView v){
 
             for (int j = 0; j < 5; j++) {
                 if (colorButtonsArray[0] == true) {
@@ -167,32 +171,29 @@ public class MainActivity<pinArray> extends AppCompatActivity implements View.On
             }
 
 
-    }
-
-    public void colorPins(){
-        for(int i = 0; i < 3; i++){
-            if(pinArray[0] == true){
-                chooseColor(a);
-
-            }
-            if(pinArray[1] == true){
-                chooseColor(b);
-
-            }
-            if(pinArray[2] == true){
-                chooseColor(c);
-
-            }
-            if(pinArray[3] == true){
-                chooseColor(d);
-
-            }
         }
-    }
+//color the corresponding pins based on pinArray bool values
+        public void colorPins () {
+
+                if (pinArray[0] == true) {
+                    chooseColor(a);
+                }
+                if (pinArray[1] == true) {
+                    chooseColor(b);
+                }
+                if (pinArray[2] == true) {
+                    chooseColor(c);
+                }
+                if (pinArray[3] == true) {
+                    chooseColor(d);
+                }
+
+        }
 
 
-
-        public void clearColorArray (int value){
+        //only one color choice can be active at a time
+        //therefore we clear all data except the inserted color in this moment.
+        public void clearColorArray ( int value){
             for (int i = 0; i < 6; i++) {
                 if (value == i) {
                     //do nothing
@@ -202,70 +203,27 @@ public class MainActivity<pinArray> extends AppCompatActivity implements View.On
         }
 
 
-
+        //on click on checkbox update screen
         @Override
         public void onClick (View v){
+
             checkPins();
-            checkColor();
+            setColor();
             System.out.println(Arrays.toString(pinArray));
             System.out.println(Arrays.toString(colorButtonsArray));
             colorPins();
         }
 
+
         @Override
         public void onCheckedChanged (RadioGroup group,int checkedId){
-            switch (group.getId()) {
-                case R.id.radioGroupID:
 
-                    checkPins();
-                    checkColor();
-                    System.out.println(Arrays.toString(pinArray));
-                    System.out.println(Arrays.toString(colorButtonsArray));
-                    colorPins();
-
-
-
-            }
+            checkPins();
+            setColor();
+            System.out.println(Arrays.toString(pinArray));
+            System.out.println(Arrays.toString(colorButtonsArray));
+            colorPins();
         }
 
-        @Override
-        public void onCheckedChanged (CompoundButton buttonView ,boolean isChecked){
-            switch (buttonView.getId()) {
-                case R.id.checkBoxAID:
 
-                    checkPins();
-                    checkColor();
-                    System.out.println(Arrays.toString(pinArray));
-                    System.out.println(Arrays.toString(colorButtonsArray));
-
-                    colorPins();
-
-
-                case R.id.checkBoxBID:
-                    checkPins();
-                    checkColor();
-                    System.out.println(Arrays.toString(pinArray));
-                    System.out.println(Arrays.toString(colorButtonsArray));
-
-                    colorPins();
-
-
-                case R.id.checkBoxCID:
-                    checkPins();
-                    checkColor();
-                    System.out.println(Arrays.toString(pinArray));
-                    System.out.println(Arrays.toString(colorButtonsArray));
-
-                    colorPins();
-
-
-                case R.id.checkBoxDID:
-                    checkPins();
-                    checkColor();
-                    System.out.println(Arrays.toString(pinArray));
-                    System.out.println(Arrays.toString(colorButtonsArray));
-
-                    colorPins();
-            }
-        }
     }
